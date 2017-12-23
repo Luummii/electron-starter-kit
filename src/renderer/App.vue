e4fd5ca2d7a79b7f7e7e0c11ed9be19a8c24161b
Andrey Zimin (luummii) <luummii@vuejs.fun>
2017-12-23 20:56:26 +0300

    Add files

---
+++ src/renderer/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">    
+    <router-view  ></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'notebook'
+}
+</script>
+
+<style lang="stylus">
+@import './assets/lib/normalize'
+
+@font-face
+  font-family 'Ubuntu-Bold'
+  src url('./assets/fonts/Ubuntu-Bold.woff2') format('woff2')
+  font-weight bold
+  font-style normal  
+
+body
+  font-family Ubuntu-Bold
+  font-size 30px
+</style>
---
+++ src/renderer/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <img src="../assets/img/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper
+  text-align center
+  display block
+
+  img
+    width 100px
+    height 100px
+    margin-top 200px
+
+</style>
